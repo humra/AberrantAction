f5aaecada8f400195d329ebfb7c017c72fa27792
Matija Huremović <matija.huremovic@gmail.com>
2018-04-24 14:01:00 +0200

    Player movement added

---
+++ AberrantAction/Assets/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float movementSpeed;
+    [SerializeField]
+    private float interpolation;
+
+    private Vector2 facingDirection;
+    private Rigidbody2D rb;
+	
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        facingDirection = Vector2.zero;
+	}
+	
+	
+	void Update () {
+
+        Vector2 movementVector = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal") * movementSpeed, interpolation),
+        Mathf.Lerp(0, Input.GetAxis("Vertical") * movementSpeed, interpolation));
+        rb.velocity = movementVector;
+
+
+        if (movementVector != Vector2.zero)
+        {
+            facingDirection = rb.velocity.normalized;
+        }
+        transform.up = facingDirection;
+    }
+}
