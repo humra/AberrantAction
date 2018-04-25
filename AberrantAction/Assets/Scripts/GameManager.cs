d3e0430c39b08de9167222e4163e2160afab0660
Matija Huremović <matija.huremovic@gmail.com>
2018-04-25 13:03:06 +0200

    Added player HP and HP regeneration system

---
+++ AberrantAction/Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject player;
+    [SerializeField]
+    private Text healthBar;
+    private PlayerStats playerStats;
+
+    void Start () {
+        playerStats = player.GetComponent<PlayerStats>();
+        healthBar.text = playerStats.GetCurrentHealth().ToString();
+        InvokeRepeating("UpdateHPBar", 0.1f, 0.1f);
+    }
+	
+	
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.R))
+        {
+            DealDamageToPlayer(10);
+        }
+	}
+
+    private void DealDamageToPlayer(int damage)
+    {
+        playerStats.TakeDamage(damage);
+
+        UpdateHPBar();
+    }
+
+    public void UpdateHPBar()
+    {
+        healthBar.text = playerStats.GetCurrentHealth().ToString();
+    }
+}
---
+++ AberrantAction/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+
+    [SerializeField]
+    private int maxHP = 100;
+    [SerializeField]
+    private int regenFactorPerSecond = 1;
+    private int currentHP;
+
+	void Start () {
+        currentHP = maxHP;
+        InvokeRepeating("RegenerateHealth", 1f, 1f);
+	}
+	
+	
+	void Update () {
+		
+	}
+
+    public void TakeDamage(int damage)
+    {
+        currentHP -= damage;
+        if(currentHP < 0)
+        {
+            currentHP = 0;
+        }
+    }
+
+    public int GetCurrentHealth()
+    {
+        return currentHP;
+    }
+
+    private void RegenerateHealth()
+    {
+        if(currentHP < maxHP)
+        {
+            currentHP += regenFactorPerSecond;
+
+            if(currentHP > maxHP)
+            {
+                currentHP = maxHP;
+            }
+        }
+    }
+}
