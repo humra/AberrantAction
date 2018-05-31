2d8eedb19ab4afe5f22e1d4a08448bffe141428a
Matija Huremović <matija.huremovic@gmail.com>
2018-05-31 11:50:02 +0200

    GameManager optimization

--- AberrantAction/Assets/Scripts/GameManager.cs
+++ AberrantAction/Assets/Scripts/GameManager.cs
@@ -18,12 +18,6 @@ public class GameManager : MonoBehaviour {
     private PlayerController playerController;
     private BossController bossController;
     [SerializeField]
-    private GameObject[] enemySpawnPoints;
-    [SerializeField]
-    private Enemy[] enemyTypes;
-    [SerializeField]
-    private float spawnRate = 1f;
-    [SerializeField]
     private GameObject gameOverScreen;
     [SerializeField]
     private GameObject pauseScreen;
@@ -40,11 +34,6 @@ public class GameManager : MonoBehaviour {
         playerHealthBar.text = playerStats.GetCurrentHealth().ToString();
         bossHealthBar.text = bossController.GetCurrentHealth().ToString();
         InvokeRepeating("UpdateHPBar", 0.1f, 0.1f);
-
-        if(SceneManager.GetActiveScene().name.Equals("TestingLevel"))
-        {
-            InvokeRepeating("SpawnEnemyInTestingLevel", 1f, spawnRate);
-        }
     }
 	
 	void Update () {
@@ -61,40 +50,6 @@ public class GameManager : MonoBehaviour {
     {
         playerHealthBar.text = playerStats.GetCurrentHealth().ToString();
         bossHealthBar.text = bossController.GetCurrentHealth().ToString();
-    }
-
-    private void SpawnEnemyInTestingLevel()
-    {
-        if(bossController.GetNumberOfEnemies() >= enemySpawnPoints.Length)
-        {
-            return;
-        }
-
-        int position;
-        int counter = 0;
-        do
-        {
-            position = Random.Range(0, enemySpawnPoints.Length);
-            counter += 1;
-        } while (enemySpawnPoints[position].transform.childCount > 0 && counter < enemySpawnPoints.Length);
-
-        Enemy instance = Instantiate(enemyTypes[Random.Range(0, enemyTypes.Length)], enemySpawnPoints[position].transform);
-
-        Vector3 aimTarget = bossController.transform.position;
-        aimTarget.z = 0f;
-        Vector3 objPos = enemySpawnPoints[position].transform.position;
-        aimTarget.x = aimTarget.x - objPos.x;
-        aimTarget.y = aimTarget.y - objPos.y;
-        float angle = Mathf.Atan2(aimTarget.y, aimTarget.x) * Mathf.Rad2Deg;
-
-        instance.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
-
-        if(Random.Range(0, 2) % 2 == 0)
-        {
-            instance.GetComponent<Enemy>().SetWillDropHealthGlobe(true);
-        }
-
-        bossController.AddNewEnemy(instance);
     }
 
     public void GameOver(string cause)
---
+++ AberrantAction/Assets/Scripts/TestingLevelSpawner.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingLevelSpawner : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject[] enemySpawnPoints;
+    [SerializeField]
+    private Enemy[] enemyTypes;
+    [SerializeField]
+    private float spawnRate = 1f;
+    private BossController bossController;
+
+
+    private void Start()
+    {
+        bossController = FindObjectOfType<BossController>();
+        InvokeRepeating("SpawnEnemyInTestingLevel", 1f, spawnRate);
+    }
+
+    private void SpawnEnemyInTestingLevel()
+    {
+        if (bossController.GetNumberOfEnemies() >= enemySpawnPoints.Length)
+        {
+            return;
+        }
+
+        int position;
+        int counter = 0;
+        do
+        {
+            position = Random.Range(0, enemySpawnPoints.Length);
+            counter += 1;
+        } while (enemySpawnPoints[position].transform.childCount > 0 && counter < enemySpawnPoints.Length);
+
+        Enemy instance = Instantiate(enemyTypes[Random.Range(0, enemyTypes.Length)], enemySpawnPoints[position].transform);
+
+        Vector3 aimTarget = bossController.transform.position;
+        aimTarget.z = 0f;
+        Vector3 objPos = enemySpawnPoints[position].transform.position;
+        aimTarget.x = aimTarget.x - objPos.x;
+        aimTarget.y = aimTarget.y - objPos.y;
+        float angle = Mathf.Atan2(aimTarget.y, aimTarget.x) * Mathf.Rad2Deg;
+
+        instance.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
+
+        if (Random.Range(0, 2) % 2 == 0)
+        {
+            instance.GetComponent<Enemy>().SetWillDropHealthGlobe(true);
+        }
+
+        bossController.AddNewEnemy(instance);
+    }
+}
